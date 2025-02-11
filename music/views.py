ab28a69322f6c99a99c764a70f3a9950e8fc421f
Preston Willen <prestonwillen@gmail.com>
2025-02-11 17:53:25 -0500

    add database connection to view. HTML should be templated in the future

--- music/views.py
+++ music/views.py
@@ -1,10 +1,19 @@
 from django.http import HttpResponse
 
+from music.models import Album
+
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>This is the Music app homepage</h1>")
+    all_albums = Album.objects.all()
+
+    html = ''
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+
+    return HttpResponse(html)
 
 
 def detail(request, album_id):
