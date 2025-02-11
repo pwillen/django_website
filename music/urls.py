7978f45ee8c8489de2011e8eaa84b0253de24470
Preston Willen <prestonwillen@gmail.com>
2025-02-11 17:47:13 -0500

    add another detail view

--- music/urls.py
+++ music/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from music import views
 
 urlpatterns = [
+    # /music/
     path('', views.index, name='index'),
+    # /music/712/
+    path('<int:album_id>/', views.detail, name='detail'),
 ]
